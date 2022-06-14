0c5f95796da6eba97bbc3fc7683a3af5a9341d82
Elliot Harding <elliot@hardings.org.uk>
2022-06-14 22:38:35 +0100

    Added Navbar Component, started sketching out navbar look

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import React, {Component} from 'react';
+import Navbar from './components/NavBar'
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+}
+from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar/>
+            </div>
+        </Router>
+);
 }
 
 export default App;
